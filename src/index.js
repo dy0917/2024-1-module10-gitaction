b71463188edd607679314260ac2948563a8647db
Kingsley <kingsley.huang@mycare.org.nz>
2024-06-29 11:44:19 +1200

    add changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello Kingsley! build with git action');
+  res.send(`Hello Kingsley! build with git action ${process.env.HELLO_WORLD}`);
 });
 
 
