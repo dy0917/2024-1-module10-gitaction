bae8e4134fc626fda6a270598a20acea4009cbe4
Kingsley <kingsley.huang@mycare.org.nz>
2024-06-29 12:07:27 +1200

    update process

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send(`Hello Kingsley! build with git action ${process.env.HELLO_WORLD}`);
+  res.send(
+    `Hello Kingsley! build with git action ${process.env.HELLO_WORLD} ${prcess.env.API_KEY}`
+  );
 });
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
